70c09c9c7a63fb78d0eaabaa5c6aafb780330fc2
Facundo Vinitzca <facundo@vinitzca.com.ar>
2026-05-09 17:00:50 -0300

    #3 - creo .txt entrada para pruebas iniciales

--- main.c
+++ main.c
@@ -1,7 +1,23 @@
-#include <stdio.h> 
+#include <stdio.h>
 
-int main(int argc, char *argv[]) 
-{ 
-    printf("Hola Mundo\n"); 
-    return 0; 
+int main(int argc, char *argv[])
+{
+    FILE *archivo_entrada, *archivo_salida;
+    char* conjunto_palabras = "";
+    int prueba;
+    archivo_entrada = fopen("entrada.txt", "r"); 
+    archivo_salida = fopen("entrada.txt", "w");
+    if (archivo_entrada != NULL)
+    {
+        fscanf(archivo_entrada,"%d", &prueba);
+        printf("El conjunto de palabras es: %d" + prueba);
+        printf("Hola mundo");
+        fclose(archivo_entrada);
+    }
+    else
+    {
+        printf("El archivo entrada.txt no ha podido abrirse");
+    }
+
+    return 0;
 }
