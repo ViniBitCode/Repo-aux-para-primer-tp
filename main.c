c2f6d5dae2bea600698fe7c4c92c63494e4bed77
Facundo Vinitzca <facundo@vinitzca.com.ar>
2026-05-09 17:49:06 -0300

    #4 - Saco conjunto de palabras de entrada.txt

--- main.c
+++ main.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
     FILE *archivo_entrada, *archivo_salida;
-    char* conjunto_palabras = "";
-    int prueba;
-    archivo_entrada = fopen("entrada.txt", "r"); 
-    archivo_salida = fopen("entrada.txt", "w");
+
+    archivo_entrada = fopen("archivos .txt/entrada.txt", "r");
+    char palabras_a_analizar[256]; // Ver otra manera de hacer strings en C para que realmente se puedan meter de longitud desconocida
+
+    archivo_salida = fopen("archivos .txt/salida.txt", "w");
+
     if (archivo_entrada != NULL)
     {
-        fscanf(archivo_entrada,"%d", &prueba);
-        printf("El conjunto de palabras es: %d" + prueba);
-        printf("Hola mundo");
+        fscanf(archivo_entrada, "%255s", palabras_a_analizar);
+        printf("El conjunto de palabras es: %s", palabras_a_analizar);
+
+        // Debatir forma para desglosar las palabras...
+        // . Ir sacando de a 1 en base a la coma y meterlas en una estrucutura tipo Pila para LIFO
+        //      . Propongo LIFO por como muestra en las imagenes la manera en la que salen las palabras
+
         fclose(archivo_entrada);
+        fclose(archivo_salida);
     }
     else
     {
