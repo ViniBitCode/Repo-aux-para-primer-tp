cf0347a95bc91116a055586a941778e461babeec
Facundo Vinitzca <facundo@vinitzca.com.ar>
2026-05-10 12:55:20 -0300

    #5 - Impresion de palabras pore consola sin pila

--- main.c
+++ main.c
@@ -7,18 +7,22 @@ int main(int argc, char *argv[])
     FILE *archivo_entrada, *archivo_salida;
 
     archivo_entrada = fopen("archivos .txt/entrada.txt", "r");
-    char palabras_a_analizar[256]; // Ver otra manera de hacer strings en C para que realmente se puedan meter de longitud desconocida
-
+    int caracter;
     archivo_salida = fopen("archivos .txt/salida.txt", "w");
 
     if (archivo_entrada != NULL)
     {
-        fscanf(archivo_entrada, "%255s", palabras_a_analizar);
-        printf("El conjunto de palabras es: %s", palabras_a_analizar);
-
-        // Debatir forma para desglosar las palabras...
-        // . Ir sacando de a 1 en base a la coma y meterlas en una estrucutura tipo Pila para LIFO
-        //      . Propongo LIFO por como muestra en las imagenes la manera en la que salen las palabras
+        while ((caracter = fgetc(archivo_entrada)) != EOF)
+        {
+            if (caracter != ',')
+            {
+                printf("%c", caracter); // Imprimir carácter por carácter
+            }
+            else
+            {
+                printf("\n");
+            }
+        }
 
         fclose(archivo_entrada);
         fclose(archivo_salida);
